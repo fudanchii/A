51b5b6f07112cb43882a3791228090c4d102205f
Nurahmadie <nurahmadie@gmail.com>
2014-05-01 10:31:37 +0700

    re-add testing

--- a.go
+++ a.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"github.com/fudanchii/A/b"
-	"github.com/fudanchii/A/c"
 )
 
 func main() {
 	b.Bbb()
-	c.TestBbb()
 }
--- c/c_test.go
+++ c/c_test.go
@@ -2,14 +2,16 @@ package c
 
 import (
 	"fmt"
-	"github.com/fudanchii/a/b"
+	"github.com/fudanchii/A/b"
+
+	"testing"
 )
 
-func TestBbb() {
+func TestBbb(t *testing.T) {
 	a := true
 	b.Bbb()
 	fmt.Println("Ccc")
 	if !a {
-		println("false assertion")
+		t.Error("false assertion")
 	}
 }
