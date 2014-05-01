3fe8752ee6fcd35fc1d89b39d15a6bf961dff7ac
Nurahmadie <nurahmadie@gmail.com>
2014-05-01 09:49:42 +0700

    remove testing

--- a.go
+++ a.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/fudanchii/A/b"
+import (
+	"github.com/fudanchii/A/b"
+	"github.com/fudanchii/A/c"
+)
 
 func main() {
 	b.Bbb()
+	c.TestBbb()
 }
--- c/c_test.go
+++ c/c_test.go
@@ -1,17 +1,15 @@
-package b_test
+package c
 
 import (
 	"fmt"
 	"github.com/fudanchii/a/b"
-	"testing"
 )
 
-func TestBbb(t *testing.T) {
+func TestBbb() {
 	a := true
 	b.Bbb()
 	fmt.Println("Ccc")
 	if !a {
 		println("false assertion")
-		t.Errorf("false assertion")
 	}
 }
